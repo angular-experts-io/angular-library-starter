39ea242312ec07f8044e3c4db5647e7924c7db81
kreuzerk <kevin.kreuzer90@icloud.com>
2023-01-09 08:32:34 +0100

    fix: 🐛 customization - adjust releaserc

--- tools/customization.mjs
+++ tools/customization.mjs
@@ -121,7 +121,7 @@ function renameKebabCases(libraryName) {
 
     const replaceOptions = {
         ignore: 'node_modules/**/*',
-        files: '**/*.{ts,html,json,scss,js}',
+        files: '{**/*.{ts,html,json,scss,js},.releaserc}',
         from: new RegExp(LIBRARY_NAME, 'g'),
         to: kebabcase(libraryName)
     }
